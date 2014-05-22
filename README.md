06e04806ad3c10c3df60a1f9f4b1b020c15b948f
Rob Schluntz <saitou1024@gmail.com>
2014-05-22 16:26:08 -0400

    website

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-TODO: Website.
+TODO: crontab.
---
+++ html/test.php
@@ -0,0 +1,10 @@
+#!/usr/bin/env php-cgi
+
+<html>
+<body>
+
+Welcome <?php echo $_GET["email"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?>
+
+</body>
+</html>
