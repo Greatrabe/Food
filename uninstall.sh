3a585e450a6529ac2f6ddbabd02ba3b83ce08f7b
Rob <rschlunt@uwaterloo.ca>
2015-01-14 13:27:50 -0500

    backs up Userdata before uninstalling

--- uninstall.sh
+++ uninstall.sh
@@ -6,6 +6,9 @@ mesg() {
 	echo $@ 1>&2
 }
 
+mesg "Backing up Userfile."
+cp ~/public_html/Food/html/Userfile .
+
 mesg "Removing ~/public_html/Food"
 rm -rf ~/public_html/Food ~/public_html/uw-food
 
